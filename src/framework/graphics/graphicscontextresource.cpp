e5f17c178600a96321be9ed3ae58612298ddb7db
gered <gered@blarg.ca>
2013-04-01 17:45:40 -0400

    add checks to help prevent duplicate initialization of GraphicsContextResource

--- src/framework/graphics/graphicscontextresource.cpp
+++ src/framework/graphics/graphicscontextresource.cpp
@@ -21,12 +21,20 @@ void GraphicsContextResource::Release()
 BOOL GraphicsContextResource::Initialize()
 {
 	STACK_TRACE;
+	ASSERT(m_graphicsDevice == NULL);
+	if (m_graphicsDevice != NULL)
+		return FALSE;
+	
 	return TRUE;
 }
 
 BOOL GraphicsContextResource::Initialize(GraphicsDevice *graphicsDevice)
 {
 	STACK_TRACE;
+	ASSERT(m_graphicsDevice == NULL);
+	if (m_graphicsDevice != NULL)
+		return FALSE;
+	
 	ASSERT(graphicsDevice != NULL);
 	if (graphicsDevice == NULL)
 		return FALSE;
